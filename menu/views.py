fd350174274d893c53186af27f0b72bd9a7975ca
Jean-Benoit Drouin-Cloutier <jbenoit.drouin@gmail.com>
2021-09-23 21:15:41 -0400

    api rest pour récupérer les pizzas

--- menu/views.py
+++ menu/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Pizza
+from django.core import serializers
 
 # Create your views here.
 
 
 def index(request):
     pizzas = Pizza.objects.all().order_by('prix')
-    # pizzas_names_prices = [f"{pizza.nom} : {pizza.prix}$" for pizza in pizzas]
-    # pizzas_names_prices_str = ", ".join(pizzas_names_prices)
-    # return HttpResponse(f"Les pizzas : {pizzas_names_prices_str}")
     return render(request, 'menu/index.html', {'pizzas': pizzas})
+
+
+def api_get_pizzas(request):
+    pizzas = Pizza.objects.all().order_by('prix')
+    json = serializers.serialize("json", pizzas)
+    return HttpResponse(json)
